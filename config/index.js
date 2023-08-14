6baa203081f5f2d941dc5db68fa48470449c7cb9
pikaCode144 <1447881964@qq.com>
2023-08-14 20:36:42 +0800

    配置环境变量

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
-export const GHOSTURL = 'http://localhost:2368'
-export const GHOSTKEY = 'b31bc1be89ca250e3a0569a030'
-export const GHOSTVERSION = 'v5.58.0'
+export const GHOSTURL = process.env.GHOSTURL || 'http://localhost:2368'
+export const GHOSTKEY = process.env.GHOSTKEY || 'b31bc1be89ca250e3a0569a030'
+export const GHOSTVERSION = process.env.GHOSTVERSION || 'v5.58.0'
